ce36bbe20dc581d15a3a6cf5880606b00513748e
FrozenPI-XEL <leon.ayvazi4n@gmail.com>
2025-09-07 20:59:34 +0200

    Update

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,15 +3,17 @@ import "../../global.css";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 
+const isLoggedIn = false; 
+
 export default function RootLayout() {
   return (
     <React.Fragment>
       <StatusBar style="auto" />
       <Stack>
-        <Stack.Protected guard={true}>
+        <Stack.Protected guard={isLoggedIn}>
           <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
         </Stack.Protected>
-        <Stack.Protected guard={false}>
+        <Stack.Protected guard={!isLoggedIn}>
           <Stack.Screen name="login"/>
         </Stack.Protected> 
       </Stack>
