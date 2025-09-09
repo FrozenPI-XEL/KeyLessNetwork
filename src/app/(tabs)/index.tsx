56d69df69b52b77acdd4779942ea9524ed19b09d
FrozenPI-XEL <leon.ayvazi4n@gmail.com>
2025-09-09 23:24:03 +0200

    Gute Nacht schnell einpaar unnötige Changes

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,9 +4,12 @@ import React from "react";
 
 export default function IndexScreen() {
   return (
-    <View className="justify-center flex-1 p-4 bg-slate-900" >
+    <View className="justify-center flex-col p-4 bg-slate-900" >
       <Text className="text-white font-bold text-xl text-center">
-        Home Screen //TODO: implement home screen
+        Willkommen im MakerLab
+      </Text>
+      <Text text-gray-400 text-sm mt-1 ml-2>
+        founded by Digiclub e.V 
       </Text>
     </View>
   );
