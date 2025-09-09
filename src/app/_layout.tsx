c489687e8d11377f919a6dec7fc599e961d249c5
FrozenPI-XEL <leon.ayvazi4n@gmail.com>
2025-09-09 11:53:30 +0200

    Update
    Login Bug behoben. Auth funktioniert super.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,26 +2,41 @@ import { Stack } from "expo-router";
 import "../../global.css";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { FadeIn } from "react-native-reanimated";
 import { useAuthStore } from "../utils/authStore";
 
 export default function RootLayout() {
-  const {isLoggedIn, isadmin, istempadmin,iswhitecard} = useAuthStore();
+  const { isLoggedIn, isadmin } = useAuthStore();
+
   return (
-    <React.Fragment>
+    <>
       <StatusBar style="auto" />
       <Stack>
-        <Stack.Protected guard={isLoggedIn}>
-          <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+        <Stack.Protected guard={!isLoggedIn}>
+          <Stack.Screen
+            name="login"
+            options={{
+              title: "Anmelden",
+              headerStyle: { backgroundColor: "#0f172b" },
+              headerTintColor: "#fff",
+              headerTitleStyle: { fontWeight: "bold" },
+              headerTitleAlign: "center",
+            }}
+          />
         </Stack.Protected>
-          <Stack.Screen name="login" options={{
-            title:"Anmelden",
-            headerStyle: { "backgroundColor": "#0f172b" },
-            headerTintColor: "#fff",
-            headerTitleStyle: {fontWeight: "bold"},
-            headerTitleAlign: "center",
-            }}/>
+
+        <Stack.Protected guard={isLoggedIn}>
+          <Stack.Screen
+            name="(tabs)"
+            options={{ headerShown: false }}
+          />
+          {isadmin && (
+            <Stack.Screen
+              name="admin"
+              options={{ title: "Admin Bereich" }}
+            />
+          )}
+        </Stack.Protected>
       </Stack>
-    </React.Fragment>
+    </>
   );
 }
